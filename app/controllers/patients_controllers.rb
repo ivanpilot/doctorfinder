3e0474cda0d13c9822caeb75d5803403f8721d04
Ivan Pilot <ivanpilot@gmail.com>
2016-07-26 23:34:48 +0100

    implemented profile and profile update for patients

--- app/controllers/patients_controllers.rb
+++ app/controllers/patients_controllers.rb
@@ -9,4 +9,27 @@ class PatientsController < ApplicationController
     end
   end
 
+  get "/patients/:slug/profile" do
+    if is_logged_in? && user_type? == "patient"
+      erb :'patients/profile_show'
+    else
+      redirect to "/"
+    end
+  end
+
+  get "/patients/:slug/profile/edit" do
+    if is_logged_in? && user_type? == "patient"
+      erb :'patients/profile_edit'
+    else
+      redirect to "/"
+    end
+  end
+
+  patch "/patients/:slug/profile/edit" do
+    patient = Patient.find_by_slug(params[:slug])
+    patient.update(params[:patient])
+    patient.save
+    redirect to "/patients/#{patient.slug}/profile"
+  end
+
 end
